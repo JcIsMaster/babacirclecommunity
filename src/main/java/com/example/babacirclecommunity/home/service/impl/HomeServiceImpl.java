269c426fec15716992a24e19043edf3edcce9233
JclsMaste <jinclol@163.com>
2021-05-23 14:40:20 +0800

    2

--- src/main/java/com/example/babacirclecommunity/home/service/impl/HomeServiceImpl.java
+++ src/main/java/com/example/babacirclecommunity/home/service/impl/HomeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.babacirclecommunity.home.service.impl;
 
 import com.example.babacirclecommunity.circle.dao.AttentionMapper;
+import com.example.babacirclecommunity.circle.dao.CircleGiveMapper;
 import com.example.babacirclecommunity.circle.dao.CircleMapper;
+import com.example.babacirclecommunity.circle.dao.CommentMapper;
 import com.example.babacirclecommunity.circle.vo.CircleClassificationVo;
 import com.example.babacirclecommunity.common.constanct.CodeType;
 import com.example.babacirclecommunity.common.exception.ApplicationException;
@@ -43,6 +45,12 @@ public class HomeServiceImpl implements IHomeService {
 
     @Autowired
     private CircleMapper circleMapper;
+
+    @Autowired
+    private CircleGiveMapper circleGiveMapper;
+
+    @Autowired
+    private CommentMapper commentMapper;
 
     @Autowired
     private AttentionMapper attentionMapper;
@@ -87,10 +95,46 @@ public class HomeServiceImpl implements IHomeService {
 
         //查圈子
         if(strata==1){
-            List<CircleClassificationVo> classificationVos = circleMapper.queryFuzzyCircle(postingName,sql);
-            return classificationVos;
+            List<CircleClassificationVo> circles = circleMapper.queryFuzzyCircle(postingName,sql);
+            for (int i = 0; i < circles.size(); i++) {
+                //得到图片组
+                String[] strings = circleMapper.selectImgByPostId(circles.get(i).getId());
+                circles.get(i).setImg(strings);
+
+                //得到点过赞人的头像
+                String[] strings1 = circleGiveMapper.selectCirclesGivePersonAvatar(circles.get(i).getId());
+                circles.get(i).setGiveAvatar(strings1);
+
+                //得到点赞数量
+                Integer integer1 = circleGiveMapper.selectGiveNumber(circles.get(i).getId());
+                circles.get(i).setGiveNumber(integer1);
+
+                //等于0在用户没有到登录的情况下 直接设置没有点赞
+                if (userId == 0) {
+                    circles.get(i).setWhetherGive(0);
+                    circles.get(i).setWhetherAttention(0);
+                } else {
+                    //查看我是否关注了此人
+                    int i1 = attentionMapper.queryWhetherAttention(userId, circles.get(i).getUId());
+                    if (i1 > 0) {
+                        circles.get(i).setWhetherAttention(1);
+                    }
+
+                    //查询是否对帖子点了赞   0没有 1有
+                    Integer integer = circleGiveMapper.whetherGive(userId, circles.get(i).getId());
+                    if (integer > 0) {
+                        circles.get(i).setWhetherGive(1);
+                    }
+                }
+
+                //得到帖子评论数量
+                Integer integer2 = commentMapper.selectCommentNumber(circles.get(i).getId());
+                circles.get(i).setNumberPosts(integer2);
+            }
+            return circles;
         }
-        return null;
+        //业务异常
+        throw new ApplicationException(CodeType.SERVICE_ERROR);
     }
 
     @Override
