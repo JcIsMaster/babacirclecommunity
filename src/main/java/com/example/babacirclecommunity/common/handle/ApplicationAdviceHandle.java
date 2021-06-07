2daff7e6974c6abd40b686e8ad20a1330fe6d52a
LMQ <2798712275@example.com>
2021-06-07 15:16:05 +0800

    解决redis连接超时 更换连接池

--- src/main/java/com/example/babacirclecommunity/common/handle/ApplicationAdviceHandle.java
+++ src/main/java/com/example/babacirclecommunity/common/handle/ApplicationAdviceHandle.java
@@ -4,7 +4,6 @@ package com.example.babacirclecommunity.common.handle;
 import com.example.babacirclecommunity.common.constanct.CodeType;
 import com.example.babacirclecommunity.common.data.Result;
 import com.example.babacirclecommunity.common.exception.ApplicationException;
-import io.lettuce.core.RedisCommandTimeoutException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MultipartException;
@@ -56,9 +55,5 @@ public class ApplicationAdviceHandle extends ResponseEntityExceptionHandler {
         return new Result(-1,"上传单个文件大小只能上传30M","");
     }
 
-    @ExceptionHandler(value = RedisCommandTimeoutException.class)
-    Result arithmeticExceptionException(RedisCommandTimeoutException e,HttpServletRequest request){
-        return new Result(-1,"redis连接超时");
 
-    }
 }
